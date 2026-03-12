c05f41262b4a9e17550fe57213ff1b20613e3458
bencurtis2003 <1bencurtis@gmail.com>
2026-03-12 13:39:39 -0700

    fix: remove last @vercel/kv from admin files

--- pages/api/admin/reset-standings.js
+++ pages/api/admin/reset-standings.js
@@ -1,4 +1,5 @@
-import { kv } from "@vercel/kv";
+import { Redis } from "@upstash/redis";
+const kv = new Redis({ url: process.env.KV_REST_API_URL, token: process.env.KV_REST_API_TOKEN });
 
 export default async function handler(req, res) {
   if(req.headers.authorization !== `Bearer ${process.env.CRON_SECRET}`)
